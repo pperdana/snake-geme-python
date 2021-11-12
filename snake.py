5b63f8bfc419c2eafd8bd947803c6fb3a79ba429
Panji <panji9502@gmail.com>
2021-11-13 03:28:31 +0700

    Animating the snake segments using OOP

--- snake.py
+++ snake.py
@@ -14,12 +14,18 @@ class Snake():
         self.head = self.segments[0]
 
     def create_segments(self):
-        for segment in range(3):
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(START_POSITION[segment])
-            self.segments.append(new_segment)
+        for position in START_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
