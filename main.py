9eaf7e42f3277982f3295330913b83effa4aa82b
Panji <panji9502@gmail.com>
2021-11-13 01:32:59 +0700

    Animating the snake segments using OOP

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.08)
     screen.update()
     snake.move()
+    time.sleep(0.1)
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 
 START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
-
+FORWARD = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 class Snake():
     def __init__(self):
         self.segments = []
         self.create_segments()
+        self.head = self.segments[0]
 
     def create_segments(self):
         for segment in range(3):
@@ -21,4 +26,22 @@ class Snake():
             x_pos = self.segments[seg_num - 1].xcor()
             y_pos = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(x=x_pos, y=y_pos)
-        self.segments[0].forward(20)
+        self.head.forward(FORWARD)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
