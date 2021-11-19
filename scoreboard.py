4618600650d7d31000d4dc5f8a604590d8d16b7b
Panji <panji9502@gmail.com>
2021-11-19 21:51:26 +0700

    save the high score at data.txt using with parameter

--- scoreboard.py
+++ scoreboard.py
@@ -9,17 +9,24 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
