4be2fbe60a65c9e800478840b188bca2d8d45458
Panji <panji9502@gmail.com>
2021-11-19 16:06:20 +0700

    upadate script

--- food.py
+++ food.py
@@ -3,16 +3,17 @@ import random
 
 
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.color("blue")
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
         self.refresh()
 
     def refresh(self):
-        self.speed("fastest")
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- snake.py
+++ snake.py
@@ -1,24 +1,25 @@
 from turtle import Turtle
-
-START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
-FORWARD = 20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
-RIGHT = 0
 LEFT = 180
+RIGHT = 0
 
-class Snake():
+
+class Snake:
+
     def __init__(self):
         self.segments = []
-        self.create_segments()
+        self.create_snake()
         self.head = self.segments[0]
 
-    def create_segments(self):
-        for position in START_POSITION:
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
             self.add_segment(position)
 
     def add_segment(self, position):
-        new_segment = Turtle(shape="square")
+        new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
@@ -28,11 +29,11 @@ class Snake():
         self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segments)-1, 0, -1):
-            x_pos = self.segments[seg_num - 1].xcor()
-            y_pos = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(x=x_pos, y=y_pos)
-        self.head.forward(FORWARD)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
@@ -42,12 +43,10 @@ class Snake():
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
-
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
